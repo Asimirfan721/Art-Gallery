3749fa7e55ef44167fcbd03fa9029242f4be6ab8
Asimirfan721 <rajputasim721@gmail.com>
2025-03-30 15:02:16 -0300

    home controller updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,5 +9,8 @@ class HomeController extends Controller
     public function index() {
         return view('home'); // home.blade.php will be called here 
     } 
+    public function about() {
+        return view('about'); // about.blade.php will be called here 
+    }
      
 }
