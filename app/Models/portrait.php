85ed9aa5a092efb7216a8acf999962ff492726eb
Asimirfan721 <rajputasim721@gmail.com>
2025-03-30 15:11:09 -0300

    all models updated

--- app/Models/portrait.php
+++ app/Models/portrait.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class portrait extends Model
+class portrait extends Model  // Model name should be in singular form
+// and should be in PascalCase
 {
     use HasFactory; // factory has to be used
     protected $fillable = ['title','company','location','website','email','description','tags']; // fillable fields are defined here
