8c85117011f8850799dd5e3f1ad1e943e70dfec6
Asimirfan721 <rajputasim721@gmail.com>
2025-03-30 15:07:02 -0300

    portrait controller updated

--- app/Http/Controllers/portraitController.php
+++ app/Http/Controllers/portraitController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class portraitController extends Controller
 {
-    // here is funcitoned called portrait here
+    public function index()
+    {
+        return view('portrait.index'); // portrait.index will be called here 
+    }
+    
+    public function about()
+    {
+        return view('portrait.about'); // portrait.about will be called here 
+    }
 }
