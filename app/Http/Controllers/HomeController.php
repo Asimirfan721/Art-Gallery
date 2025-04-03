14b42c6bdb2c38e992e77ac16adb49b39acf3d99
Asimirfan721 <rajputasim721@gmail.com>
2025-04-02 23:15:16 -0300

    path view defined

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\View; // view path
 
 class HomeController extends Controller
 {
