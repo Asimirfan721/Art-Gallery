1b6db99ed633a128ee93509679fe32c59fd7f00c
Asimirfan721 <rajputasim721@gmail.com>
2025-04-23 20:39:16 +0500

    index added

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class StockController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('stock.index'); // stock.index will be called here 
+    }
 }
