7c8e7d783fa7378f4648b3ea7eca586dc2625525
Asimirfan721 <rajputasim721@gmail.com>
2025-04-29 23:57:56 +0500

    function updated

--- app/Http/Controllers/portraitController.php
+++ app/Http/Controllers/portraitController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class portraitController extends Controller
 {
-    public function index()
+    public function index()     //  index function
     {
         return view('portrait.index'); // portrait.index will be called here 
     }
