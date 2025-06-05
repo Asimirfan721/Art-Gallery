e26370cde1b34623971fe0e451a713f9b9de06ae
Asimirfan721 <rajputasim721@gmail.com>
2025-06-05 23:42:45 +0800

    home controller retunred

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,22 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View; // view path
 
 class HomeController extends Controller
 {
-    public function index() {
-        return View::make('home'); // home.blade.php will be called here      
-    } 
-    public function about() {
-        return view('about'); // about.blade.php will be called here 
-    }    
-    public function contact() {
-        return view('contact'); // contact.blade.php will be called here 
-    }
-    public function create() {
-        return View::make('home'); // home.blade.php will be called here      
-    } 
-
-      
+    //
 }
