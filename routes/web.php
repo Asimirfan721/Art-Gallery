8f606a3c0f66eca6482377448b942368d1a5d952
Asimirfan721 <rajputasim721@gmail.com>
2025-06-21 22:05:56 +0500

    project up and running after issues

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use App\Http\Controllers\HomeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/',[ListingController::class, 'index']);
